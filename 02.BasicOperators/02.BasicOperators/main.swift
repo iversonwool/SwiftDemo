b320cf43db0194a435d7c4e2463573ab7bfe92ce
李浩 <iversonwool@sina.com>
2018-08-24 17:09:15 +0800

    nil coalescing operator: ??

--- 02.BasicOperators/02.BasicOperators/main.swift
+++ 02.BasicOperators/02.BasicOperators/main.swift
@@ -33,6 +33,32 @@ print("Hello, " + name + "!")
 // no self increase decrease operators //
 
 
+// 空合运算符 Nil Coalescing Operator
+//
+//空合运算符（a ?? b）将对可选类型 a 进行空判断，如果 a 包含一个值就进行解封，否则就返回一个默认值 b。
+//表达式 a 必须是 Optional 类型。默认值 b 的类型必须要和 a 存储值的类型保持一致。
+
+
+//a ?? b
+// |
+// V
+//a != nil ? a! : b
+
+//注意
+//
+//如果 a 为非空值（non-nil），那么值 b 将不会被计算。这也就是所谓的短路求值。
+
+let defaultColorName = "red"
+var userDefinedColorName: String?
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+print(colorNameToUse) // red
+
+userDefinedColorName = "green"
+colorNameToUse = userDefinedColorName ?? defaultColorName
+print(colorNameToUse) // green
+
+
+
 // [1, 4)
 for i in 1..<4 {
     print(i)
