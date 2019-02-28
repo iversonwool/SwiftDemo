3d053673f0e83517ec77e515b1f8d0b1d52d8c02
LeeHow <iversonwool@sina.com>
2019-02-28 16:39:38 +0800

    运算符重载

--- 26.AdvancedOperators/26.AdvancedOperators/main.swift
+++ 26.AdvancedOperators/26.AdvancedOperators/main.swift
@@ -19,7 +19,7 @@ print("Hello, World!")
 //所有的这些溢出运算符都是以 & 开头的。
 
 
-//位运算符
+//MARK: - 位运算符
 
 //
 //按位取反运算符（~）可以对一个数值的全部比特位进行取反：
@@ -66,17 +66,21 @@ print(u_254)
 
 
 
-//溢出运算符
+//MARK: - 溢出运算符
 //溢出加法 &+
 //溢出减法 &-
 //溢出乘法 &*
 //
 //
 
-//运算符函数
-//类和结构体可以为现有的运算符提供自定义的实现，这通常被称为运算符重载。
-//
-//
+
+//MARK: - 优先级和结合性
+
+//MARK: - 运算符函数
+
+
+
+
 
 
 struct Vector2D {
@@ -86,69 +90,98 @@ struct Vector2D {
 
 
 extension Vector2D {
+    
+    
+    //MARK: - 运算符重载
+    //类和结构体可以为现有的运算符提供自定义的实现，这通常被称为运算符重载。
+
+    
+    //MARK: - 中缀运算符
+    //算术加法运算符是一个二元运算符，因为它是对两个值进行运算，同时它还可以称为中缀（zhong zhui）运算符，因为它出现在两个值中间。
+    
     static func + (left: Vector2D, right: Vector2D) -> Vector2D {
         return Vector2D(x: left.x + right.x, y: left.y + right.y)
     }
     
+    //MARK: - 前缀和后缀运算符
+    //要实现前缀或者后缀运算符，需要在声明运算符函数的时候在 func 关键字之前指定 prefix 或者 postfix 修饰符：
     static prefix func - (vector: Vector2D) -> Vector2D {
         return Vector2D(x: -vector.x, y: -vector.y)
     }
     
+    //MARK: - 复合赋值运算符
     static func += (left: inout Vector2D, right: Vector2D) {
         left = left + right
     }
 }
-
-
-
 
 let vector1 = Vector2D(x: 3.0, y: 4.0)
 let vector2 = Vector2D(x: 4.0, y: 5.0)
 let vector  = vector1 + vector2
 print(vector)
 
-
-
-
-
-
-//前缀 后缀运算符
-//要实现前缀或者后缀运算符，需要在声明运算符函数的时候在 func 关键字之前指定 prefix 或者 postfix 修饰符：
-
-
-
-//复合赋值运算符
-
-
-//extension Vector2D {
-//
-//}
-
-
-
-
 var original = Vector2D(x: 1.0, y: 2.0)
 let vectorToAdd = Vector2D(x: 3.0, y: 3.0)
 original += vectorToAdd
 print(original)
 
-
-
-
-//
 //注意
-//
-//不能对默认的赋值运算符（=）进行重载。只有组合赋值运算符可以被重载。
-//同样地，也无法对三目条件运算符 （a ? b : c） 进行重载。
+///不能对默认的赋值运算符（=）进行重载。只有组合赋值运算符可以被重载。
+///同样地，也无法对三目条件运算符 （a ? b : c） 进行重载。
 
+
+//MARK: - 等价运算符
+//通常情况下，自定义的类和结构体没有对等价运算符进行默认实现，
+//等价运算符通常被称为相等运算符（==）与不等运算符（!=）
+
+
+extension Vector2D: Equatable {
+    static func ==(left: Vector2D, right: Vector2D) -> Bool {
+        return left.x == right.x && left.y == right.y
+    }
+}
+
+//通常情况下你并不需要自己再去实现“不等”运算符（!=）。
+//标准库对于“不等”运算符提供了默认的实现，它简单地将“相等”运算符的结果进行取反后返回。
 
 //
 //Swift 为以下自定义类型提等价运算符供合成实现：
 //
 //只拥有遵循 Equatable 协议存储属性的结构体；
+
+struct Vector3D: Equatable {
+    var x = 0.0, y = 0.0, z = 0.0
+    
+}
 //只拥有遵循 Equatable 协议关联类型的枚举；
 //没有关联类型的枚举。
 
+
+//MARK: - 自定义运算符
+//新的运算符要使用 operator 关键字在***全局作用域***内进行定义，
+//同时还要***指定 prefix、infix 或者 postfix 修饰符***：
+
+
+prefix operator +++
+
+extension Vector2D {
+    static prefix func +++(vector: inout Vector2D) -> Vector2D {
+        vector += vector
+        return vector
+    }
+}
+
+//中缀运算符需要指定优先级
+//https://developer.apple.com/documentation/swift/swift_standard_library/operator_declarations
 infix operator +-: AdditionPrecedence
 
+extension Vector2D {
+    static func +-(left: Vector2D, right: Vector2D) -> Vector2D {
+        return Vector2D(x: left.x + right.x, y: left.y - right.y)
+    }
+}
 
+//注意
+
+//当定义前缀与后缀运算符的时候，我们并没有指定优先级。
+//然而，如果对同一个值同时使用前缀与后缀运算符，则后缀运算符会先参与运算。
